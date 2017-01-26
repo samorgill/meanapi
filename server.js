cbbacf2b87c965aa96c6744f4bbaa882ea838b78
sam.orgill <sam.orgill@ultra-as.com>
2017-01-26 17:56:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// BASE SETUP
+
+//call the packages we need
+
+var express = require('express');           //call express
+var app = express();                        //define our app using express
+var bodyParser = require('body-parser');
+
+//configure app to use bodyParser()
+//this will let us get the data from POST
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        //set the port
+
+//ROUTES FOR OUR API
+
+var router = express.Router();              // get an instance of the Router
+
+router.get('/', function(req, res){
+
+    res.json({ message: 'Horray! welcome to our API' });
+});
+
+//More routes to follow
+
+// REGISTER OUR ROUTES
+// all of our routes will be prefixed with /API
+app.use('/api', router);
+console.log('Magic happens on port ' + port);
