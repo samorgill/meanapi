ee76e73f942ae5c93eb64372cc4dad7b5d52a81e
sam.orgill <sam.orgill@ultra-as.com>
2017-01-26 18:13:13 +0000

    started adding mongoose

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var express = require('express');           //call express
 var app = express();                        //define our app using express
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bears');
+
 //configure app to use bodyParser()
 //this will let us get the data from POST
 
